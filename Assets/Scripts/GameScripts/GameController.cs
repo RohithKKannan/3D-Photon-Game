cae123f7b8e00fcb567df64141c9e3765c7ab681
RohithKannan <51419779+RohithKKannan@users.noreply.github.com>
2023-07-20 17:18:20 +0530

    Added a timer to the game that is synced between all clients

--- Assets/Scripts/GameScripts/GameController.cs
+++ Assets/Scripts/GameScripts/GameController.cs
@@ -3,12 +3,22 @@ using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using System;
 
 public class GameController : MonoBehaviourPunCallbacks
 {
+    private ExitGames.Client.Photon.Hashtable customValue;
+    private bool timerStarted;
+    private double startTime;
+    private double timerIncrementValue;
+    private double timerValue = 120;
+    private double timeToDisplay;
+
     [SerializeField] private PhotonView view;
     [SerializeField] private PlayerController playerPrefab;
     [SerializeField] private GameObject startButton;
+
+    [SerializeField] private TMP_Text timer;
 
     [Header("Player List")]
     [SerializeField] private GameObject lobbyPanel;
@@ -26,8 +36,30 @@ public class GameController : MonoBehaviourPunCallbacks
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            view.RPC("SpawnAllPlayers", RpcTarget.AllViaServer);
             startButton.SetActive(false);
+            customValue = new ExitGames.Client.Photon.Hashtable();
+
+            startTime = PhotonNetwork.Time;
+            customValue.Add("startTime", startTime);
+            PhotonNetwork.CurrentRoom.SetCustomProperties(customValue);
+
+            timerStarted = true;
+            timer.gameObject.SetActive(true);
+
+            view.RPC("SpawnAllPlayers", RpcTarget.AllViaServer);
+            view.RPC("SetStartTime", RpcTarget.AllViaServer);
+            startButton.SetActive(false);
+        }
+    }
+
+    [PunRPC]
+    public void SetStartTime()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            startTime = double.Parse(PhotonNetwork.CurrentRoom.CustomProperties["startTime"].ToString());
+            timerStarted = true;
+            timer.gameObject.SetActive(true);
         }
     }
 
@@ -39,7 +71,7 @@ public class GameController : MonoBehaviourPunCallbacks
         {
             if (player.IsLocal)
             {
-                Vector3 randomPos = new Vector3(Random.Range(-15, 16), 3, Random.Range(-15, 16));
+                Vector3 randomPos = new Vector3(UnityEngine.Random.Range(-15, 16), 3, UnityEngine.Random.Range(-15, 16));
 
                 object[] customPlayerData = new object[1];
                 customPlayerData[0] = MainMenuScript.playerColor;
@@ -63,6 +95,10 @@ public class GameController : MonoBehaviourPunCallbacks
 
     public void RefreshPlayerList()
     {
+        foreach (Transform item in playerListContent.transform)
+        {
+            Destroy(item.gameObject);
+        }
         foreach (Player player in PhotonNetwork.PlayerList)
         {
             GameObject newPlayerName = Instantiate(playerNamePrefab);
@@ -79,5 +115,24 @@ public class GameController : MonoBehaviourPunCallbacks
     public override void OnLeftRoom()
     {
         SceneManager.LoadScene(1);
+    }
+
+    private void UpdateTimeUI()
+    {
+        TimeSpan timeSpan = TimeSpan.FromSeconds(timeToDisplay);
+        timer.text = string.Format("{0:00}:{1:00}", timeSpan.Minutes, timeSpan.Seconds);
+    }
+
+    private void Update()
+    {
+        if (!timerStarted)
+            return;
+
+        timerIncrementValue = PhotonNetwork.Time - startTime;
+        timeToDisplay = timerValue - timerIncrementValue;
+        UpdateTimeUI();
+
+        if (timeToDisplay == 0)
+            Debug.Log("Game over!");
     }
 }
